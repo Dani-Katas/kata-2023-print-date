49057f5fdb8265845b4a0c189dbb06f78d3e8c74
Daniel Ramos <danielramosacosta1@gmail.com>
2023-02-24 17:53:46 +0000

    solve first part

--- src/test/java/org/example/PrintDateTest.java
+++ src/test/java/org/example/PrintDateTest.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 
 public class PrintDateTest {
   @Test
   public void printDate() throws Exception {
-    PrintDate printDate = new PrintDate(new Calendar(), new Printer());
+    Calendar calendar = spy(Calendar.class); // query (stub)
+    when(calendar.today()).thenReturn(new Date(0));
+    Printer printer = mock(Printer.class); // command (spy)
+    PrintDate printDate = new PrintDate(calendar, printer);
 
     printDate.printCurrentDate();
 
-    // How can we test this function?
+    verify(printer).printLine("Thu Jan 01 00:00:00 WET 1970");
   }
 }
